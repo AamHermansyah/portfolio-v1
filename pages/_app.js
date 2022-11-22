ea6489a4eef885752ac077dfd432750357682516
Aam Hermansyah <74630284+AamHermansyah@users.noreply.github.com>
2022-11-23 06:16:05 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
                 <meta name="author" content="Aam Hermansyah" />
                 <meta name="copyright" content="aamhermansyah" />
                 <meta name="description" content="Welcome to my website, you can see my portfolio and hire me." key="desc" />
-                <meta property="og:title" content="Aam Hermansyah | Developer & Development" />
+                <meta property="og:title" content="Aam Hermansyah | Javascript Developer & Designer" />
                 <meta property="og:description" content="Welcome to my website, you can see my portfolio and hire me." />
                 <meta property="og:image" content="https://drive.google.com/uc?export=view&id=14kb7D-9SfWwYua6vyuU0amNeblOSZ00F"/>
                 <meta property="og:image" itemprop="image" content="https://drive.google.com/uc?export=view&id=14kb7D-9SfWwYua6vyuU0amNeblOSZ00F" />
