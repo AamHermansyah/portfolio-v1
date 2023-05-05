2f69920aea37c19ce5fc59275269aec644e81ccd
Aam Hermansyah <74630284+AamHermansyah@users.noreply.github.com>
2023-05-05 15:23:32 +0700

    Update data.js

--- data.js
+++ data.js
@@ -188,7 +188,7 @@ export const options = [
 
 export const categories = [
     { label: 'Frontend (Integrate & Slicing)', value: 'Frontend (Integrate & Slicing)' },
-    { label: 'Frontend (Slicing)', value: 'Frontend (Slicing)' },|
+    { label: 'Frontend (Slicing)', value: 'Frontend (Slicing)' },
     { label: 'Frontend (Integrate)', value: 'Frontend (Integrate)' },
     { label: 'Frontend (Convert)', value: 'Frontend (Convert)' },
     { label: 'Backend', value: 'Backend' },
